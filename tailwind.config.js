011eb0029bb07e2261e7c9db4b1717cb24bbcf62
Rafiw Idrissu <dev.rafiu@gmail.com>
2024-05-28 16:27:07 +0000

    refactor: remove boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        xl: "1200px",
+      },
+    },
+
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "rgba(var(--background),<alpha-value>)",
+
+        foreground: "rgba(var(--foreground),<alpha-value>)",
+        "muted-foreground": "rgba(var(--muted-foreground),<alpha-value>)",
+
+        primary: "rgba(var(--clr-primary),<alpha-value>)",
       },
     },
   },
