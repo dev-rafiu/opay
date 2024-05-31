9fd65227de9a3b5877186dd35c508f3d06676071
sadiquex <abubakasaddik1@gmail.com>
2024-05-31 12:08:07 +0000

    feat: added fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,26 @@
-import { Inter } from "next/font/google";
+import { Prosto_One, Rubik, Poppins } from "next/font/google";
 import "./globals.css";
 
 import Header from "./components/header";
+import Footer from "./components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+const rubik = Rubik({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-rubik",
+});
+
+const prosto_one = Prosto_One({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-prosto-one",
+});
 
 export const metadata = {
   title: "Opay re-design",
@@ -13,11 +30,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${rubik.variable} ${prosto_one.variable} ${poppins.variable}`}
+      >
         <div className="container">
           <Header />
 
           {children}
+
+          <Footer />
         </div>
       </body>
     </html>
